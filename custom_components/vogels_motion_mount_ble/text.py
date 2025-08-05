9165966965734ae0583a33aa15418f90ebc1bb73
Nailik <13292441+Nailik@users.noreply.github.com>
2025-08-05 23:10:59 +0200

    finish translate name

--- custom_components/vogels_motion_mount_ble/text.py
+++ custom_components/vogels_motion_mount_ble/text.py
@@ -9,20 +9,16 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from . import VogelsMotionMountBleConfigEntry
 from .base import VogelsMotionMountBleBaseEntity
-from .const import DOMAIN, HA_SERVICE_DEVICE_ID, HA_SERVICE_NAME_ID, HA_SERVICE_SET_NAME
+from .const import DOMAIN, HA_SERVICE_NAME_ID, HA_SERVICE_SET_NAME
 from .coordinator import VogelsMotionMountBleCoordinator
 from .preset_base import VogelsMotionMountBlePresetBaseEntity
+from .utils import get_coordinator
 
 _LOGGER = logging.getLogger(__name__)
 
-async def _set_name(call: ServiceCall) -> None:
-    _LOGGER.debug("set_distance_service called with data: %s", call.data)
-    device_registry = dr.async_get(call.hass)
-    device = device_registry.async_get(call.data[HA_SERVICE_DEVICE_ID])
-    entry_id = next(iter(device.config_entries))
-    coordinator: VogelsMotionMountBleCoordinator = call.hass.data[DOMAIN].get(entry_id)
 
-    await coordinator.api.set_name(call.data[HA_SERVICE_NAME_ID])
+async def _set_name(call: ServiceCall) -> None:
+    await get_coordinator(call).set_name(call.data[HA_SERVICE_NAME_ID])
 
 
 async def async_setup_entry(
@@ -31,50 +27,43 @@ async def async_setup_entry(
     async_add_entities: AddEntitiesCallback,
 ):
     """Set up the Sensors."""
-    # This gets the data update coordinator from the config entry runtime data as specified in your __init__.py
     coordinator: VogelsMotionMountBleCoordinator = config_entry.runtime_data.coordinator
-
-
+    # register services
     hass.services.async_register(
         DOMAIN,
         HA_SERVICE_SET_NAME,
         _set_name,
     )
 
-    # Enumerate all the sensors in your data value from your DataUpdateCoordinator and add an instance of your sensor class
-    # to a list for each one.
-    # This maybe different in your specific case, depending on how your data is structured
-    numbers = [NameText(coordinator)]  + [PresetNameText(coordinator, preset_index) for preset_index in range(7)]
+    # register entities
+    async_add_entities(
+        [NameText(coordinator)]
+        + [PresetNameText(coordinator, preset_index) for preset_index in range(7)]
+    )
 
-    # Create the sensors.
-    async_add_entities(numbers)
 
 class NameText(VogelsMotionMountBleBaseEntity, TextEntity):
-    """Implementation of a sensor."""
+    """Implementation of a the Name Text."""
 
-    _attr_native_min = 1
-    _attr_native_max = 20
-
-    @property
-    def name(self) -> str:
-        """Return the name of the sensor."""
-        return "name"
-
-    @property
-    def unique_id(self) -> str:
-        """Return unique id."""
-        return "name"
+    def __init__(self, coordinator: VogelsMotionMountBleCoordinator) -> None:
+        """Initialise entity."""
+        self._attr_name = "Name"
+        self._attr_unique_id = "name"
+        self._attr_native_min = 1
+        self._attr_native_max = 20
+        super().__init__(coordinator)
 
     @property
     def native_value(self):
         """Return the state of the entity."""
-        if not self.coordinator.data:
+        if self.coordinator.data is None:
             return None
         return self.coordinator.data.name
 
     async def async_set_value(self, value: str) -> None:
-        """Set the value from the UI."""
+        """Set the name value from the UI."""
         await self.coordinator.api.set_name(value)
+
 
 class PresetNameText(VogelsMotionMountBlePresetBaseEntity, TextEntity):
     """Implementation of a sensor."""
@@ -90,8 +79,8 @@ class PresetNameText(VogelsMotionMountBlePresetBaseEntity, TextEntity):
     def name(self) -> str:
         """Return the name of the sensor."""
         if self._preset:
-            return  f"Preset {self._preset_name} Name"
-        return  f"Preset {self._preset_index} Name"
+            return f"Preset {self._preset_name} Name"
+        return f"Preset {self._preset_index} Name"
 
     @property
     def unique_id(self) -> str:
@@ -107,4 +96,4 @@ class PresetNameText(VogelsMotionMountBlePresetBaseEntity, TextEntity):
 
     async def async_native_set_value(self, value: str) -> None:
         """Set the value from the UI."""
-        await self.coordinator.api.set_preset(preset_id = self._preset_index, name = value)
+        await self.coordinator.api.set_preset(preset_id=self._preset_index, name=value)
