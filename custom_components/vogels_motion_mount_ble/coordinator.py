61ce20021dbe75799d41f3c5fb22cb1268c9879a
Nailik <13292441+Nailik@users.noreply.github.com>
2025-11-13 23:26:15 +0100

    fix availability crash

--- custom_components/vogels_motion_mount_ble/coordinator.py
+++ custom_components/vogels_motion_mount_ble/coordinator.py
@@ -83,10 +83,14 @@ class VogelsMotionMountBleCoordinator(DataUpdateCoordinator[VogelsMotionMountDat
 
     def _available_callback(self, info: BluetoothServiceInfoBleak, change: BluetoothChange) -> None:
         _LOGGER.debug("%s is discovered again", info.address)
+        if(self.data is None): # may be called before data is available
+            return
         self.async_set_updated_data(replace(self.data, available=True))
 
     def _unavailable_callback(self, info: BluetoothServiceInfoBleak) -> None:
         _LOGGER.debug("%s is no longer seen", info.address)
+        if(self.data is None): # may be called before data is available
+            return
         self.async_set_updated_data(replace(self.data, available=False))
 
     async def unload(self):
