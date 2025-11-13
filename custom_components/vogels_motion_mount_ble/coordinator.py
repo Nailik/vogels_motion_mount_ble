b6a0759cac059178e5376a74996a525a7add015f
Nailik <13292441+Nailik@users.noreply.github.com>
2025-11-13 23:35:34 +0100

    add tests for availability update

--- custom_components/vogels_motion_mount_ble/coordinator.py
+++ custom_components/vogels_motion_mount_ble/coordinator.py
@@ -84,6 +84,7 @@ class VogelsMotionMountBleCoordinator(DataUpdateCoordinator[VogelsMotionMountDat
     def _available_callback(self, info: BluetoothServiceInfoBleak, change: BluetoothChange) -> None:
         _LOGGER.debug("%s is discovered again", info.address)
         if(self.data is None): # may be called before data is available
+            self._async_update_data() # load the data
             return
         self.async_set_updated_data(replace(self.data, available=True))
 
